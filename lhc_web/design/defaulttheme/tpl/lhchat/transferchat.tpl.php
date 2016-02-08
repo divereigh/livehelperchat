c69836a77f1e8293cdf5396f1154f2d449a405ea
Damian Ivereigh <damo@launtel.net.au>
2016-02-09 09:59:24 +1100

    In simplified layout, remove all the selection in chat transfer that allow people to show hidden, offline or disabled departments

--- lhc_web/design/defaulttheme/tpl/lhchat/transferchat.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/transferchat.tpl.php
@@ -42,6 +42,7 @@
 
             		<input type="button" onclick="lhinst.transferChatDep('<?php echo $chat->id;?>')" class="btn btn-default" value="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Transfer');?>" />
         		</div>
+<?php if ((int)erLhcoreClassModelChatConfig::fetch('simplified_layout')->current_value == 0) : ?>
         		<div class="col-xs-6">
         		    <div class="checkbox">
         		      <label><input type="checkbox" onchange="updateTransferDepartments()" checked="checked" id="dep_transfer_only_explicit"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Only departments which are online and explicitly assigned operator are online');?></label>
@@ -55,6 +56,7 @@
         		      <label><input type="checkbox" onchange="updateTransferDepartments()" checked="checked" id="dep_transfer_exclude_disabled"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/transferchat','Exclude disabled departments');?></label>
         		    </div>
         		</div>
+<?php endif; ?>
             </div>
 
             <script type="text/javascript">
