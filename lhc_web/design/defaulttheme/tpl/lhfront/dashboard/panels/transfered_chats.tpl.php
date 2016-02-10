9ce25bc025028c131c8194db9e4d888b8d5b3411
Damian Ivereigh <damo@launtel.net.au>
2016-02-10 15:53:24 +1100

    Didn't do the mergeing quite right for the simplified layout

--- lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/transfered_chats.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhfront/dashboard/panels/transfered_chats.tpl.php
@@ -26,12 +26,12 @@
 					</td>			
        		   	</tr>
 			</table>
-			<div ng-if="transfer_chats.list.length == 0" class="m10 alert alert-info">
-<?php if ((int)erLhcoreClassModelChatConfig::fetch('simplified_layout')->current_value == 0) : ?>
-			<i class="material-icons">search</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Nothing found')?>...
-<?php endif; ?>
-				&nbsp;
 			</div>
+		<div ng-if="transfer_chats.list.length == 0" class="m10 alert alert-info">
+<?php if ((int)erLhcoreClassModelChatConfig::fetch('simplified_layout')->current_value == 0) : ?>
+		<i class="material-icons">search</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Nothing found')?>...
+<?php endif; ?>
+			&nbsp;
 		</div>
 	</div>
 </div>
