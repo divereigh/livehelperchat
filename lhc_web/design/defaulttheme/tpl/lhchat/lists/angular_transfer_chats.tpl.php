e59fa3d768775f574953ec770011b3020fb42a88
Damian Ivereigh <damo@launtel.net.au>
2016-03-06 21:29:10 +1100

    Remove the open new window option in the transfer right panel and make the name the link to click to accept the chat.

--- lhc_web/design/defaulttheme/tpl/lhchat/lists/angular_transfer_chats.tpl.php
+++ lhc_web/design/defaulttheme/tpl/lhchat/lists/angular_transfer_chats.tpl.php
@@ -1,9 +1,11 @@
 <ul class="list-unstyled">
     <li ng-repeat="chat in transfer_chats.list" ng-class="{'icon-user-away': chat.user_status_front == 2, 'icon-user-online': chat.user_status_front == 0}">
 	<a class="material-icons" title="ID - {{chat.id}}, {{chat.user_name}}" ng-click="lhc.previewChat(chat.id)" >info_outline</a>
-	<a class="right-action-hide material-icons" ng-click="lhc.startChatTransfer(chat.id,chat.nick,chat.transfer_id)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Accept chat');?>">chat</a>
+	<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhchat','singlechatwindow')) : ?>
 	<a class="material-icons" ng-click="lhc.startChatNewWindowTransfer(chat.id,chat.nick,chat.transfer_id)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Open in a new window');?>">open_in_new</a>
-	{{chat.id}}. {{chat.nick}} ({{chat.time_front}})
+	<?php endif; ?>
+	<a ng-click="lhc.startChatTransfer(chat.id,chat.nick,chat.transfer_id)" title="<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('chat/syncadmininterface','Accept chat');?>">{{chat.nick}}</a>
+	<small><i>{{chat.time_front}}</i></small>
     </li>
 </ul>
 <p ng-show="transfer_chats.list.length == 0"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Empty...');?></p>
